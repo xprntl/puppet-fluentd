b88544abafcfa009f71c37459266eb7b2794857a
xprntl <denis.s.sorokin@gmail.com>
2016-03-02 17:59:04 -0800

    Update yum.pp

--- manifests/install_repo/yum.pp
+++ manifests/install_repo/yum.pp
@@ -9,7 +9,7 @@ class fluentd::install_repo::yum (
 
     # Sorry for the different naming of the Repository between debian and redhat.
     # But I dont want rename it to avoid a duplication.
-    yumrepo { 'TreasureData':
+    yumrepo { 'treasure-data':
         descr => 'Treasure Data',
         baseurl='http://packages.treasuredata.com/2/redhat/$releasever/$basearch'
         gpgkey='https://packages.treasuredata.com/GPG-KEY-td-agent'
